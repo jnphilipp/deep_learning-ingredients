b05572a3e5b4ffece944af4dd033771405a21861
jnphilipp <mail@jnphilipp.org>
2018-02-26 14:45:31 +0100

    Update ingredients.data.json module for multiple models.

--- data/json.py
+++ data/json.py
@@ -9,7 +9,7 @@ from keras.utils import np_utils
 
 
 @ingredients.capture
-def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None):
+def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None, y_null=False):
     trans = str.maketrans(dict((s, str(i)) for i, s in enumerate(vocab)))
 
     data = {}
@@ -17,52 +17,91 @@ def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None):
               'r', encoding='utf-8') as f:
         data = json.loads(f.read())
 
-    entities = []
-    y_entities = {}
+    entities = {}
+    texts = {'nlu': []}
+    y = {'nlu': {'intent': [], 'language': []}}
     for example in data['common_examples']:
         for entity in example['entities']:
-            if entity['entity'] not in entities:
-                entities.append(entity['entity'])
-                y_entities[entity['entity']] = []
+            entity = entity['entity']
+            if entity['parent']:
+                name = entity['parent']
+                child = entity['name']
+                texts[name] = []
+            else:
+                name = entity['name']
+                child = None
+
+            if name not in entities:
+                entities[name] = None
+                y['nlu'][name] = []
+
+            if child and (entities[name] is None or child not in entities[name]):
+                if entities[name] is None:
+                    entities[name] = ['null']
+                entities[name].append(child)
 
     input_len = 0
-    intents = []
-    languages = []
-    texts = []
-    y_intent = []
-    y_language = []
+    intents = ['null'] if y_null else []
+    languages = ['null'] if y_null else []
     for example in data['common_examples']:
         text = fclean(example['text'], vocab) if fclean else example['text']
         input_len = max(input_len, len(text))
-        texts.append([int(s.translate(trans)) for s in '%s^' % text])
+        texts['nlu'].append([int(s.translate(trans)) for s in '%s^' % text])
 
         if example['intent'] not in intents:
             intents.append(example['intent'])
         if example['language'] not in languages:
             languages.append(example['language'])
 
-        y_intent.append(intents.index(example['intent']))
-        y_language.append(languages.index(example['language']))
+        y['nlu']['intent'].append(intents.index(example['intent']))
+        y['nlu']['language'].append(languages.index(example['language']))
+
+        names = [e['entity']['parent'] if e['entity']['parent']
+                 else e['entity']['name'] for e in example['entities']]
 
         for entity in entities:
-            if True in [entity == e['entity'] for e in example['entities']]:
-                y_entities[entity].append(1)
+            if True in [entity == n for n in names]:
+                y['nlu'][entity].append(1)
             else:
-                y_entities[entity].append(0)
+                y['nlu'][entity].append(0)
 
-    X = np.zeros((len(texts), input_len + 1))
-    for i, text in enumerate(texts):
-        X[i][0:len(text)] = text
+        for k in texts.keys():
+            if k == 'nlu':
+                continue
 
-    y = {
-        'intent': np_utils.to_categorical(y_intent, len(intents)),
-        'language': np_utils.to_categorical(y_language, len(languages)),
-    }
-    for k, v in y_entities.items():
-        y[k] = np.asarray(v).reshape((len(X), 1))
+            for entity in example['entities']:
+                if entity['entity']['parent'] is None:
+                    continue
+                elif entity['entity']['parent'] != k:
+                    continue
+
+                if k not in y:
+                    y[k] = []
+
+                texts[k].append(texts['nlu'][-1])
+                y[k].append(entities[entity['entity']['parent']].index(
+                    entity['entity']['name'])
+                )
+
+    X = {k: np.zeros((len(v), input_len + 1)) for k, v in texts.items()}
+    for k in texts.keys():
+        for i, text in enumerate(texts[k]):
+            X[k][i][0:len(text)] = text
+
+    for k in y.keys():
+        if k == 'nlu':
+            y[k]['intent'] = np_utils.to_categorical(y[k]['intent'], len(intents))
+            y[k]['language'] = np_utils.to_categorical(y[k]['language'], len(languages))
+            for e, v in y[k].items():
+                if e == 'intent' or e == 'language':
+                    continue
+                y[k][e] = np.asarray(v).reshape((len(X['nlu']), 1))
+        else:
+            y[k] = np_utils.to_categorical(y[k], len(entities[k]))
+
     return X, y, len(vocab), {
         'intent': intents,
         'language': languages,
         'entities': entities,
-        'outputs': list(y.keys())
+        'outputs': list(y['nlu'].keys())
     }
