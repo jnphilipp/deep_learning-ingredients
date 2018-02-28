cc97ca483746c2b8be1aef04b05ed9dc2ce66b21
jnphilipp <mail@jnphilipp.org>
2018-02-28 10:34:02 +0000

    Update ingredients.data.json module.

--- data/json.py
+++ data/json.py
@@ -9,7 +9,7 @@ from keras.utils import np_utils
 
 
 @ingredients.capture
-def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None, y_null=False):
+def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None):
     trans = str.maketrans(dict((s, str(i)) for i, s in enumerate(vocab)))
 
     data = {}
@@ -18,90 +18,75 @@ def load(DATASETS_DIR, dataset, which_set, vocab, fclean=None, y_null=False):
         data = json.loads(f.read())
 
     entities = {}
-    texts = {'nlu': []}
-    y = {'nlu': {'intent': [], 'language': []}}
+    input_len = 0
+    outputs = {'intent': ['null'], 'language': ['null']}
+    texts = []
+    y = {'intent': [], 'language': []}
     for example in data['common_examples']:
+        text = fclean(example['text'], vocab) if fclean else example['text']
+        input_len = max(input_len, len(text))
+        texts.append([int(s.translate(trans)) for s in '%s^' % text])
+
+        if example['intent'] not in outputs['intent']:
+            outputs['intent'].append(example['intent'])
+        if example['language'] not in outputs['language']:
+            outputs['language'].append(example['language'])
+
         for entity in example['entities']:
             entity = entity['entity']
             if entity['parent']:
                 name = entity['parent']
                 child = entity['name']
-                texts[name] = []
             else:
                 name = entity['name']
                 child = None
 
             if name not in entities:
-                entities[name] = None
-                y['nlu'][name] = []
+                entities[name] = []
+                outputs[name] = ['null'] if child else []
+                y[name] = []
 
-            if child and (entities[name] is None or child not in entities[name]):
-                if entities[name] is None:
-                    entities[name] = ['null']
+            if child and child not in entities[name]:
                 entities[name].append(child)
+                outputs[name].append(child)
 
-    input_len = 0
-    intents = ['null'] if y_null else []
-    languages = ['null'] if y_null else []
+        y['intent'].append(outputs['intent'].index(example['intent']))
+        y['language'].append(outputs['language'].index(example['language']))
+
     for example in data['common_examples']:
-        text = fclean(example['text'], vocab) if fclean else example['text']
-        input_len = max(input_len, len(text))
-        texts['nlu'].append([int(s.translate(trans)) for s in '%s^' % text])
-
-        if example['intent'] not in intents:
-            intents.append(example['intent'])
-        if example['language'] not in languages:
-            languages.append(example['language'])
-
-        y['nlu']['intent'].append(intents.index(example['intent']))
-        y['nlu']['language'].append(languages.index(example['language']))
-
         names = [e['entity']['parent'] if e['entity']['parent']
                  else e['entity']['name'] for e in example['entities']]
+        children = [e['entity']['name'] for e in example['entities']
+                    if e['entity']['parent']]
 
-        for entity in entities:
-            if True in [entity == n for n in names]:
-                y['nlu'][entity].append(1)
-            else:
-                y['nlu'][entity].append(0)
-
-        for k in texts.keys():
-            if k == 'nlu':
-                continue
-
+        for k, v in entities.items():
+            value = 0
             for entity in example['entities']:
-                if entity['entity']['parent'] is None:
-                    continue
-                elif entity['entity']['parent'] != k:
-                    continue
+                name = entity['entity']['name']
+                parent = entity['entity']['parent']
+                if k == name and len(v) == 0:
+                    value = 1
+                elif k == parent and len(v) > 0:
+                    value = outputs[parent].index(name)
+            y[k].append(value)
 
-                if k not in y:
-                    y[k] = []
-
-                texts[k].append(texts['nlu'][-1])
-                y[k].append(entities[entity['entity']['parent']].index(
-                    entity['entity']['name'])
-                )
-
-    X = {k: np.zeros((len(v), input_len + 1)) for k, v in texts.items()}
-    for k in texts.keys():
-        for i, text in enumerate(texts[k]):
-            X[k][i][0:len(text)] = text
+    X = np.zeros((len(texts), input_len + 1))
+    for i, text in enumerate(texts):
+        X[i][0:len(text)] = text
 
     for k in y.keys():
-        if k == 'nlu':
-            y[k]['intent'] = np_utils.to_categorical(y[k]['intent'], len(intents))
-            y[k]['language'] = np_utils.to_categorical(y[k]['language'], len(languages))
-            for e, v in y[k].items():
-                if e == 'intent' or e == 'language':
-                    continue
-                y[k][e] = np.asarray(v).reshape((len(X['nlu']), 1))
+        if len(outputs[k]) > 0:
+            y[k] = np_utils.to_categorical(y[k], len(outputs[k]))
         else:
-            y[k] = np_utils.to_categorical(y[k], len(entities[k]))
+            y[k] = np.asarray(y[k]).reshape((len(X), 1))
 
-    return X, y, len(vocab), {
-        'intent': intents,
-        'language': languages,
-        'entities': entities,
-        'outputs': list(y['nlu'].keys())
-    }
+    to_del = []
+    for k, v in outputs.items():
+        if len(v) == 0:
+            to_del.append(k)
+    for i in to_del:
+        del outputs[i]
+
+    outputs['entities'] = entities
+    outputs['outputs'] = list(y.keys())
+    return X, y, len(vocab), outputs
