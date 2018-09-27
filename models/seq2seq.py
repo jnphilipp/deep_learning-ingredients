1a074fffa54e0362884d571861f86c6f71bee04e
Unknown <jnphilipp@gmail.com>
2018-09-27 09:35:13 +0200

    Fix seq2seq ingredient and add bidirectional.

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -2,8 +2,8 @@
 
 from copy import deepcopy
 from keras import backend as K
-from keras.layers import (deserialize as deserialize_layer, Conv1D, Embedding,
-                          Input, RepeatVector, SpatialDropout1D)
+from keras.layers import *
+from keras.layers import deserialize as deserialize_layer
 from keras.models import Model
 from keras.optimizers import deserialize as deserialize_optimizers
 from ingredients.models import ingredients
@@ -21,7 +21,7 @@ def build(vocab_size, N, layers, outputs, optimizer, _log,
     inputs = Input(shape=(None,), name='input')
     x = Embedding.from_config(dict(layers['embedding_config'],
                                    **{'input_dim': vocab_size}))(inputs)
-    if layers['embedding_dropout']:
+    if 'embedding_dropout' in layers and layers['embedding_dropout']:
         x = SpatialDropout1D(rate=layers['embedding_dropout'])(x)
 
     if type(N) == int:
@@ -32,7 +32,13 @@ def build(vocab_size, N, layers, outputs, optimizer, _log,
         layer = deepcopy(layers['recurrent_in_config'])
         if i != N[0] - 1:
             layer['config']['return_sequences'] = True
-        x = deserialize_layer(layer)(x)
+
+        if 'bidirectional_config' in layers and layers['bidirectional_config']:
+            conf = dict(layers['bidirectional_config'],
+                        **{'layer': layer})
+            x = Bidirectional.from_config(conf)(x)
+        else:
+            x = deserialize_layer(layer)(x)
     vec = x
 
     # outputs
@@ -59,8 +65,7 @@ def build(vocab_size, N, layers, outputs, optimizer, _log,
 
             for j in range(N[i + 1]):
                 layer = deepcopy(layers['recurrent_out_config'])
-                if j != N[i + 1] - 1:
-                    layer['config']['return_sequences'] = True
+                layer['config']['return_sequences'] = True
                 x = deserialize_layer(layer)(x)
             outs.append(Conv1D.from_config(conv1d_config)(x))
 
