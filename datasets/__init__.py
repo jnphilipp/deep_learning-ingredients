4f54e9302a349a24cd96848e5157289d423fad09
J. Nathanael Philipp <nathanael@philipp.land>
2019-11-28 12:38:54 +0100

    Update datasets ingredient.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -14,20 +14,4 @@ def config():
     DATASETS_DIR = os.path.join(PROJECT_DIR, 'datasets')
 
 
-@ingredient.capture
-def get_full_dataset_path(DATASETS_DIR, dataset, train_set,
-                          validation_set=None):
-    base_dir = os.path.join(DATASETS_DIR, dataset)
-    trainset_path = os.path.join(base_dir, train_set)
-    if validation_set:
-        return trainset_path, os.path.join(base_dir, validation_set)
-    else:
-        return trainset_path, None
-
-
-from . import h5py
-from . import images
-from . import json
 from . import keras
-from . import text
-from .core import *
--- datasets/keras.py
+++ datasets/keras.py
@@ -1,54 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from keras import backend as K, datasets
-from keras.utils import to_categorical
+from logging import Logger
+from tensorflow.keras import datasets
+from tensorflow.keras.utils import to_categorical
 
 from . import ingredient
 
 
 @ingredient.capture
-def mnist(data_format=None, _log=None):
+def mnist(_log: Logger):
     _log.info('Loading MNIST.')
-
-    if data_format is None:
-        data_format = K.image_data_format()
-    if data_format not in {'channels_first', 'channels_last'}:
-        raise ValueError('Unknown data_format:', data_format)
 
     nb_classes = 10
     rows, cols = 28, 28
 
-    (X, y), (X_val, y_val) = datasets.mnist.load_data()
-    y = to_categorical(y, nb_classes)
+    (x_train, y_train), (x_val, y_val) = datasets.mnist.load_data()
+    y_train = to_categorical(y_train, nb_classes)
     y_val = to_categorical(y_val, nb_classes)
 
-    if data_format == 'channels_first':
-        X = X.reshape(X.shape[0], 1, rows, cols)
-        X_val = X_val.reshape(X_val.shape[0], 1, rows, cols)
-        input_shape = (1, rows, cols)
-    elif data_format == 'channels_last':
-        X = X.reshape(X.shape[0], rows, cols, 1)
-        X_val = X_val.reshape(X_val.shape[0], rows, cols, 1)
-        input_shape = (rows, cols, 1)
+    x_train = x_train.reshape(x_train.shape[0], rows, cols, 1)
+    x_val = x_val.reshape(x_val.shape[0], rows, cols, 1)
 
-    X = X.astype('float32')
-    X_val = X_val.astype('float32')
+    x_train = x_train.astype('float32')
+    x_val = x_val.astype('float32')
 
-    _log.info(f'{X.shape[0]} train samples.')
-    _log.info(f'{X_val.shape[0]} validation samples.')
+    _log.info(f'{x_train.shape[0]} train samples.')
+    _log.info(f'{x_val.shape[0]} validation samples.')
 
-    return X, y, X_val, y_val
+    return x_train, y_train, x_val, y_val
 
 
 @ingredient.capture
 def cifar10(_log):
     _log.info('Loading CIFAR10.')
 
-    (X, y), (X_val, y_val) = datasets.cifar10.load_data()
-    y = to_categorical(y, 10)
+    (x_train, y_train), (x_val, y_val) = datasets.cifar10.load_data()
+    y_train = to_categorical(y_train, 10)
     y_val = to_categorical(y_val, 10)
 
-    _log.info(f'{X.shape[0]} train samples.')
-    _log.info(f'{X_val.shape[0]} validation samples.')
+    _log.info(f'{x_train.shape[0]} train samples.')
+    _log.info(f'{x_val.shape[0]} validation samples.')
 
-    return X, y, X_val, y_val
+    return x_train, y_train, x_val, y_val
