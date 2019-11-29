da0423bee556bccb07358c1d386184fb63176d6c
J. Nathanael Philipp <nathanael@philipp.land>
2019-11-29 15:26:10 +0100

    Add license.

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2019 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
+#
+# This file is part of deep_learning-ingredients.
+#
+# deep_learning-ingredients is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# deep_learning-ingredients is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with deep_learning-ingredients. If not, see
+# <http://www.gnu.org/licenses/>.
 
 from sacred import Ingredient
 
