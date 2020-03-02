39b2dbe0fb6415153c921b5645097ac3e99e6b7e
J. Nathanael Philipp <nathanael@philipp.land>
2020-03-02 13:27:35 +0100

    Update translation in PrintSamplePrediction.

--- callbacks/print_sample_prediction.py
+++ callbacks/print_sample_prediction.py
@@ -22,17 +22,17 @@ from tensorflow.keras.callbacks import Callback
 
 
 class PrintSamplePrediction(Callback):
-    def __init__(self, vocab, X, y):
+    def __init__(self, X, y, translate_func):
         super(PrintSamplePrediction, self).__init__()
-        self.vocab = vocab
         self.X = X
         self.y = y
+        self.translate_func = translate_func
 
     def trans_output(self, v):
         if len(v.shape) == 1:
-            return ''.join([self.vocab[int(v.argmax())]])
+            return self.translate_func([int(v.argmax())])
         else:
-            return ''.join([self.vocab[int(l)] for l in v.argmax(axis=1)])
+            return self.translate_func([l for l in v.argmax(axis=1)])
 
     def on_epoch_end(self, epoch, logs=None):
         p = self.model.predict(self.X)
@@ -49,7 +49,7 @@ class PrintSamplePrediction(Callback):
                 outs = [self.trans_output(p[i])]
                 ys = [self.trans_output(self.y[i])]
 
-            s = f'"{"".join([self.vocab[int(l)] for l in self.X[i]])}" =>'
+            s = f'"{self.translate_func(self.X[i])}" =>'
             for o, y in zip(outs, ys):
                 s += f' "{o}":"{y}"'
             print(s)
