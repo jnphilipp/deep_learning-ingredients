1fc77e12c4c3543e48d2db983b7fa71eef0ceffe
J. Nathanael Philipp <nathanael@philipp.land>
2020-11-11 13:25:59 +0100

    Update csv import and texts generator.

--- datasets/csv.py
+++ datasets/csv.py
@@ -23,7 +23,7 @@ import numpy as np
 from csv import DictReader
 from logging import Logger
 from sacred import Ingredient
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 
 from .. import paths
 from ..vocab import Vocab
@@ -33,11 +33,13 @@ ingredient = Ingredient('datasets.csv', ingredients=[paths.ingredient])
 
 
 @ingredient.capture
-def load(path: str, fieldnames: List[Tuple[str, str, str]], paths: Dict,
-         _log: Logger, vocab: Optional[Vocab] = None, append_one: bool = False,
-         dtype: type = np.uint) -> Tuple[Dict[str, List[np.ndarray]],
-                                         Dict[str, List[np.ndarray]]]:
-    def transform(field: str, vocab: Optional[Vocab] = None) -> np.ndarray:
+def load(path: str, x_fieldnames: Union[str, List[str]],
+         y_fieldnames: Union[str, List[str]], paths: Dict, _log: Logger,
+         vocab: Optional[Vocab] = None, x_append_one: bool = True,
+         y_append_one: bool = False, dtype: type = np.uint) -> \
+        Tuple[Dict[str, List[np.ndarray]], Dict[str, List[np.ndarray]]]:
+    def transform(field: str, append_one: bool,
+                  vocab: Optional[Vocab] = None) -> np.ndarray:
         if ';' in field and ',' in field:
             return np.array([(vocab.get(j) if vocab else j)
                              for i in field.split(';') for j in i.split(',')
@@ -47,15 +49,21 @@ def load(path: str, fieldnames: List[Tuple[str, str, str]], paths: Dict,
                              for i in field.split(',') if i] +
                             ([1] if append_one else []), dtype=dtype)
 
+    if type(x_fieldnames) == str:
+        x_fieldnames = [x_fieldnames]
+    if type(y_fieldnames) == str:
+        y_fieldnames = [y_fieldnames]
+
     _log.info(f'Load {path.format(datasets_dir=paths["datasets_dir"])}.')
-    x: Dict[str, List[np.ndarray]] = {field[0]: [] for field in fieldnames}
-    y: Dict[str, List[np.ndarray]] = {field[0]: [] for field in fieldnames}
+    x: Dict[str, List[np.ndarray]] = {k: [] for k in x_fieldnames}
+    y: Dict[str, List[np.ndarray]] = {k: [] for k in y_fieldnames}
     with open(path.format(datasets_dir=paths["datasets_dir"]), 'r',
               encoding='utf8') as f:
         reader = DictReader(f, dialect='unix')
         for row in reader:
-            for field in fieldnames:
-                x[field[0]].append(transform(row[field[1]], vocab))
-                y[field[0]].append(transform(row[field[2]]))
+            for field in x_fieldnames:
+                x[field].append(transform(row[field], x_append_one, vocab))
+            for field in y_fieldnames:
+                y[field].append(transform(row[field], y_append_one, vocab))
 
     return x, y
--- datasets/texts.py
+++ datasets/texts.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 
 from logging import Logger
+from itertools import chain
 from sacred import Ingredient
 from tensorflow.keras.utils import Sequence
 from typing import Dict, List, Optional, Tuple, Union
@@ -17,189 +18,180 @@ ingredient = Ingredient('datasets.texts', ingredients=[paths.ingredient])
 
 
 class TextSequence(Sequence):
-    def __init__(self, headline: List[List[int]], lead: List[List[int]],
-                 text: List[List[int]], y_headline: List[List[int]],
-                 y_lead: List[List[int]], y_text: List[List[int]],
-                 headline_len: Optional[int] = None,
-                 lead_len: Optional[int] = None,
-                 text_len: Optional[int] = None,
-                 total_len: Optional[int] = None, batch_size: int = 10,
-                 sample_weights: bool = False, mode: str = 'separate',
-                 dtype: type = np.uint):
-        assert len(headline) == len(lead) and len(lead) == len(text) and \
-            len(text) == len(y_headline) and len(y_headline) == len(y_lead) \
-            and len(y_lead) == len(y_text)
+    def __init__(self, X: Dict[str, Tuple[int, List[List[int]]]],
+                 Y: Dict[str, Tuple[int, List[List[int]]]],
+                 batch_size: int = 10, sample_weights: bool = False,
+                 mode: str = 'separate', dtype: type = np.uint):
         assert mode in ['separate', 'concat']
 
-        self.headline = headline
-        self.lead = lead
-        self.text = text
-        self.y_headline = y_headline
-        self.y_lead = y_lead
-        self.y_text = y_text
-        self.headline_len = headline_len if headline_len else \
-            max([len(i) for i in self.headline])
-        self.lead_len = lead_len if lead_len else \
-            max([len(i) for i in self.lead])
-        self.text_len = text_len if text_len else \
-            max([len(i) for i in self.text])
-        self.total_len = total_len if total_len else \
-            max([len(headline[i]) + len(lead[i]) + len(text[i])
-                 for i in range(len(text))])
+        self.X = X
+        self.Y = Y
+        self.length = len(X[list(X.keys())[0]][1])
         self.batch_size = batch_size
         self.sample_weights = sample_weights
         self.mode = mode
         self.dtype = dtype
 
     def __len__(self) -> int:
-        return math.ceil(len(self.headline) / self.batch_size)
+        return math.ceil(self.length / self.batch_size)
 
-    def __getitem__(self, idx: int) -> Tuple[Dict[str, np.ndarray],
-                                             Dict[str, np.ndarray],
-                                             Union[Tuple[None],
-                                                   Tuple[None, None, None],
+    def __getitem__(self, idx: int) -> Union[Tuple[Dict[str, np.ndarray],
+                                                   Dict[str, np.ndarray]],
+                                             Tuple[Dict[str, np.ndarray],
+                                                   Dict[str, np.ndarray],
                                                    Dict[str, np.ndarray]]]:
-        if len(self.headline) >= (idx * self.batch_size) + self.batch_size:
+        if self.length >= (idx * self.batch_size) + self.batch_size:
             current_batch_size = self.batch_size
         else:
-            current_batch_size = len(self.headline) - (idx * self.batch_size)
+            current_batch_size = self.length - (idx * self.batch_size)
         start_idx = idx * self.batch_size
         end_idx = start_idx + current_batch_size
 
         if self.mode == 'separate':
-            headline = np.zeros((current_batch_size, self.headline_len),
-                                dtype=self.dtype)
-            lead = np.zeros((current_batch_size, self.lead_len),
-                            dtype=self.dtype)
-            text = np.zeros((current_batch_size, self.text_len),
-                            dtype=self.dtype)
-            y_headline = np.zeros((current_batch_size, self.headline_len),
-                                  dtype=self.dtype)
-            y_lead = np.zeros((current_batch_size, self.lead_len),
-                              dtype=self.dtype)
-            y_text = np.zeros((current_batch_size, self.text_len),
-                              dtype=self.dtype)
-            if self.sample_weights:
-                sample_weight_headline = np.zeros((current_batch_size,
-                                                   self.headline_len),
-                                                  dtype=np.uint)
-                sample_weight_lead = np.zeros((current_batch_size,
-                                               self.lead_len), dtype=np.uint)
-                sample_weight_text = np.zeros((current_batch_size,
-                                               self.text_len), dtype=np.uint)
-            for i, j in enumerate(range(start_idx, end_idx)):
-                headline[i, 0:len(self.headline[j])] = self.headline[j]
-                lead[i, 0:len(self.lead[j])] = self.lead[j]
-                text[i, 0:len(self.text[j])] = self.text[j]
-                y_headline[i, 0:len(self.y_headline[j])] = self.y_headline[j]
-                y_lead[i, 0:len(self.y_lead[j])] = self.y_lead[j]
-                y_text[i, 0:len(self.y_text[j])] = self.y_text[j]
-                if self.sample_weights:
-                    sample_weight_headline[i, 0:len(self.headline[j])] = 1
-                    sample_weight_lead[i, 0:len(self.lead[j])] = 1
-                    sample_weight_text[i, 0:len(self.text[j])] = 1
+            bx = {}
+            for k in self.X.keys():
+                bx[k] = np.zeros((current_batch_size, self.X[k][0]),
+                                 dtype=self.dtype)
+            by = {}
+            for k in self.Y.keys():
+                by[k] = np.zeros((current_batch_size, self.Y[k][0]),
+                                 dtype=self.dtype)
 
             if self.sample_weights:
-                return {
-                    'headline': headline,
-                    'lead': lead,
-                    'text': text,
-                }, {
-                    'pheadline': y_headline,
-                    'plead': y_lead,
-                    'ptext': y_text
-                }, {
-                    'pheadline': sample_weight_headline,
-                    'plead': sample_weight_lead,
-                    'ptext': sample_weight_text
-                }
-            else:
-                return {
-                    'headline': headline,
-                    'lead': lead,
-                    'text': text
-                }, {
-                    'pheadline': y_headline,
-                    'plead': y_lead,
-                    'ptext': y_text
-                }, (None, None, None)
-        elif self.mode == 'concat':
-            text = np.zeros((current_batch_size, self.total_len),
-                            dtype=self.dtype)
-            y_text = np.zeros((current_batch_size, self.total_len),
-                              dtype=self.dtype)
-            if self.sample_weights:
-                sample_weight_text = np.zeros((current_batch_size,
-                                               self.total_len), dtype=np.uint)
+                bw = {}
+                for k in self.X.keys():
+                    bw[k] = np.zeros((current_batch_size, self.X[k][0]),
+                                     dtype=np.uint)
+
             for i, j in enumerate(range(start_idx, end_idx)):
-                length = len(self.headline[j]) + len(self.lead[j]) + \
-                    len(self.text[j])
-                y_length = len(self.y_headline[j]) + len(self.y_lead[j]) + \
-                    len(self.y_text[j])
-                text[i, 0:length] = self.headline[j] + self.lead[j] + \
-                    self.text[j]
-                y_text[i, 0:y_length] = self.y_headline[j] + self.y_lead[j] + \
-                    self.y_text[j]
+                for k in self.X.keys():
+                    bx[k][i, 0:len(self.X[k][1][j])] = self.X[k][1][j]
+                for k in self.Y.keys():
+                    by[k][i, 0:len(self.Y[k][1][j])] = self.Y[k][1][j]
                 if self.sample_weights:
-                    sample_weight_text[i, 0:y_length] = 1
+                    for k in self.X.keys():
+                        bw[k][i, 0:len(self.X[k][1][j])] = 1
+
             if self.sample_weights:
-                return {'text': text}, {'ptext': y_text}, \
-                    {'ptext': sample_weight_text}
+                return bx, by, bw
             else:
-                return {'text': text}, {'ptext': y_text}, (None,)
+                return bx, by
+        elif self.mode == 'concat':
+            x = np.zeros((current_batch_size,
+                          sum([v[0] for v in self.X.values()])),
+                         dtype=self.dtype)
+            y = np.zeros((current_batch_size,
+                          sum([v[0] for v in self.Y.values()])),
+                         dtype=self.dtype)
+            if self.sample_weights:
+                w = np.zeros((current_batch_size,
+                              sum([v[0] for v in self.X.values()])),
+                             dtype=np.uint)
+            for i, j in enumerate(range(start_idx, end_idx)):
+                length = sum([len(v[1][j]) for v in self.X.values()])
+                y_length = sum([len(v[1][j]) for v in self.Y.values()])
+                x[i, 0:length] = list(chain(v[1][j] for v in self.X.values()))
+                y[i, 0:length] = list(chain(v[1][j] for v in self.Y.values()))
+                if self.sample_weights:
+                    w[i, 0:length] = 1
+            if self.sample_weights:
+                return x, y, w
+            else:
+                return x, y
         else:
-            return {}, {}, (None,)
+            return {}, {}
 
 
 @ingredient.capture
-def get(dataset: str, batch_size: int, mode: str, headline_fieldname: str,
-        lead_fieldname: str, text_fieldname: str, y_fieldname_prefix: str,
-        append_one: bool, sample_weights: bool, dtype: type, _log: Logger,
-        vocab_path: Optional[str] = None) -> Tuple[TextSequence, TextSequence]:
+def get(dataset: str, batch_size: int, mode: str,
+        x_fieldnames: Union[str, List[str]],
+        y_fieldnames: Union[str, List[str]], x_append_one: bool,
+        y_append_one: bool, sample_weights: bool, dtype: type, _log: Logger,
+        validation_split: Optional[float] = None,
+        vocab_path: Optional[str] = None) -> Union[TextSequence,
+                                                   Tuple[TextSequence,
+                                                         TextSequence]]:
     assert mode in ['separate', 'concat']
-
-    fieldnames = [
-        ('headline', headline_fieldname,
-         f'{y_fieldname_prefix}{headline_fieldname}'),
-        ('lead', lead_fieldname, f'{y_fieldname_prefix}{lead_fieldname}'),
-        ('text', text_fieldname, f'{y_fieldname_prefix}{text_fieldname}')
-    ]
+    assert validation_split is None or (validation_split >= 0. and
+                                        validation_split <= 1.)
 
     vocab = None
     if vocab_path:
         vocab = json.vocab(os.path.join('{datasets_dir}', dataset, vocab_path))
 
     train_csv = os.path.join('{datasets_dir}', dataset, 'train.csv')
-    train_x, train_y = csv.load(train_csv, fieldnames, vocab=vocab,
-                                append_one=append_one, dtype=dtype)
+    tx, ty = csv.load(train_csv, x_fieldnames, y_fieldnames, vocab=vocab,
+                      x_append_one=x_append_one, y_append_one=y_append_one,
+                      dtype=dtype)
+    train_x = {}
+    for k in tx.keys():
+        max_len = max([len(i) for i in tx[k]])
+        train_x[k if k.startswith('input_') else f'input_{k}'] = (max_len,
+                                                                  tx[k])
+    train_y = {}
+    for k in ty.keys():
+        max_len = max([len(i) for i in ty[k]])
+        train_y[k if k.startswith('output_') else f'output_{k}'] = (max_len,
+                                                                    ty[k])
 
     val_csv = os.path.join('{datasets_dir}', dataset, 'val.csv')
-    val_x, val_y = csv.load(val_csv, fieldnames, vocab=vocab,
-                            append_one=append_one, dtype=dtype)
+    if os.path.exists(val_csv):
+        vx, vy = csv.load(val_csv, x_fieldnames, y_fieldnames, vocab=vocab,
+                          x_append_one=x_append_one, y_append_one=y_append_one,
+                          dtype=dtype)
+        val_x = {}
+        for k in tx.keys():
+            max_len = max(train_x[k][0], max([len(i) for i in tx[k]]))
+            train_x[k] = (max_len, train_x[k][1])
+            val_x[k if k.startswith('input_') else f'input_{k}'] = (max_len,
+                                                                    vx[k])
+        val_y = {}
+        for k in ty.keys():
+            max_len = max(train_y[k][0], max([len(i) for i in ty[k]]))
+            train_y[k] = (max_len, train_y[k][1])
+            val_y[k if k.startswith('output_') else f'output_{k}'] = (max_len,
+                                                                      vy[k])
 
-    headline_len = max(max([len(i) for i in train_x['headline']]),
-                       max([len(i) for i in val_x['headline']]))
-    lead_len = max(max([len(i) for i in train_x['lead']]),
-                   max([len(i) for i in val_x['lead']]))
-    text_len = max(max([len(i) for i in train_x['text']]),
-                   max([len(i) for i in val_x['text']]))
-    total_len = max(max([len(train_x['headline'][i]) + len(train_x['lead'][i])
-                         + len(train_x['text'][i]) for i in
-                         range(len(train_x['text']))]),
-                    max([len(val_x['headline'][i]) + len(val_x['lead'][i]) +
-                         len(val_x['text'][i]) for i in
-                         range(len(val_x['text']))]))
+        for k in train_x.keys():
+            _log.info(f'X[{k}] length: {train_x[k][0]}')
+        for k in train_y.keys():
+            _log.info(f'Y[{k}] length: {train_y[k][0]}')
 
-    _log.info(f'Headline len: {headline_len}')
-    _log.info(f'Lead len: {lead_len}')
-    _log.info(f'Text len: {text_len}')
-    _log.info(f'Total len: {total_len}')
+        return TextSequence(train_x, train_y, batch_size, sample_weights,
+                            mode, dtype), \
+            TextSequence(val_x, val_y, batch_size, sample_weights, mode, dtype)
+    else:
+        for k in train_x.keys():
+            _log.info(f'X[{k}] length: {train_x[k][0]}')
+        for k in train_y.keys():
+            _log.info(f'Y[{k}] length: {train_y[k][0]}')
 
-    return TextSequence(train_x['headline'], train_x['lead'], train_x['text'],
-                        train_y['headline'], train_y['lead'], train_y['text'],
-                        headline_len, lead_len, text_len, total_len,
-                        batch_size, sample_weights, mode, dtype), \
-        TextSequence(val_x['headline'], val_x['lead'], val_x['text'],
-                     val_y['headline'], val_y['lead'], val_y['text'],
-                     headline_len, lead_len, text_len, total_len, batch_size,
-                     sample_weights, mode, dtype)
+        if validation_split:
+            rng = np.random.default_rng()
+            length = len(train_x[list(train_x.keys())[0]][1])
+            per = rng.permutation(length)
+            idx = int(length * (1. - validation_split))
+
+            _log.info(f'Making validation split, train on {idx} samples and ' +
+                      f'validating on {length - idx} samples.')
+
+            val_x = {}
+            for k in train_x.keys():
+                val_x[k] = (train_x[k][0],
+                            [train_x[k][1][i] for i in per[idx:]])
+                train_x[k] = (train_x[k][0],
+                              [train_x[k][1][i] for i in per[0:idx]])
+            val_y = {}
+            for k in train_y.keys():
+                val_y[k] = (train_y[k][0],
+                            [train_y[k][1][i] for i in per[idx:]])
+                train_y[k] = (train_y[k][0],
+                              [train_y[k][1][i] for i in per[0:idx]])
+
+            return TextSequence(train_x, train_y, batch_size, sample_weights,
+                                mode, dtype), \
+                TextSequence(val_x, val_y, batch_size, sample_weights, mode,
+                             dtype)
+        else:
+            return TextSequence(train_x, train_y, batch_size, sample_weights,
+                                mode, dtype)
